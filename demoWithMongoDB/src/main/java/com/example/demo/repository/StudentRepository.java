1add6236e7c2139bdac8f1aa72744e0af9274ab0
Dener <denerferreira-hotmart@hotmart.com>
2021-11-07 08:14:56 -0300

    Implementação de tratativa de erro Ajuste nos status de retorno da classe StudentController Ajuste no value da enum Gender

--- demoWithMongoDB/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demoWithMongoDB/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,10 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
 
     Optional<Student> findStudentByEmail(String email);
+
+    Optional<Student> findById(String id);
 
 }
